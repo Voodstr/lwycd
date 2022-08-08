876c0a3e6528f2a5a1a7325d8f5047d8eb940dd1
Diman <voods.work@yandex.ru>
2022-08-08 13:41:06 +0300

    UserInput with buttons

--- app/src/main/java/ru/voodster/lwycd/TaskScreen.kt
+++ app/src/main/java/ru/voodster/lwycd/TaskScreen.kt
@@ -1,9 +1,13 @@
 package ru.voodster.lwycd
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -11,9 +15,17 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -47,7 +59,7 @@ fun TaskScreen(checklistViewModel: ChecklistViewModel) {
 
 
 @Composable
-fun UserInput(
+fun TaskEdit(
     onTaskEdited: () -> Unit,
     resetScroll: () -> Unit,
     modifier: Modifier
@@ -55,33 +67,112 @@ fun UserInput(
     var textState by remember { mutableStateOf(TextFieldValue()) }
     // Used to decide if the keyboard should be shown
     var textFieldFocusState by remember { mutableStateOf(false) }
+    Column() {
+        TaskInputField(
+            textFieldValue = textState,
+            onTextChanged = {textState = it},
+            keyboardShown = textFieldFocusState,
+            onTextFieldFocused = { focused ->
+                if (focused) {
+                    resetScroll()
+                }
+                textFieldFocusState = focused
+            },
+            focusState = textFieldFocusState
+        )
 
-
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+            Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Save")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Add")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Delete")
+                }
+            }
+        }
+    }
 }
 
 
 @Preview
 @Composable
 fun InputFieldPreview() {
-    UserInputField()
+    Scaffold(Modifier.fillMaxSize()) {
+            TaskEdit(
+                onTaskEdited = { /*TODO*/ },
+                resetScroll = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            )
+    }
 }
 
+val KeyboardShownKey = SemanticsPropertyKey<Boolean>("KeyboardShownKey")
+var SemanticsPropertyReceiver.keyboardShownProperty by KeyboardShownKey
+
 @Composable
-fun UserInputField(textFieldValue: TextFieldValue, keyboardType: KeyboardType = KeyboardType.Text,
-                   onTextChanged: (TextFieldValue) -> Unit,
-                   keyboardShown: Boolean,
-                   onTextFieldFocused: (Boolean) -> Unit,
+fun TaskInputField(
+    textFieldValue: TextFieldValue, keyboardType: KeyboardType = KeyboardType.Text,
+    onTextChanged: (TextFieldValue) -> Unit,
+    keyboardShown: Boolean,
+    onTextFieldFocused: (Boolean) -> Unit, focusState: Boolean
 ) {
-    Row  (      modifier = Modifier
+    Row(
+        modifier = Modifier
             .fillMaxWidth()
-        .height(64.dp)
-        .semantics {
-            contentDescription = "UserInputField"
-            keyboardShownProperty = keyboardShown
-        },
-    horizontalArrangement = Arrangement.End
+            .height(64.dp)
+            .semantics {
+                contentDescription = "UserInputField"
+                keyboardShownProperty = keyboardShown
+            },
+        horizontalArrangement = Arrangement.End
     ) {
-        TextField(value = textFieldValue, onValueChange = {onTextChanged(it)})
+        Surface(Modifier.padding(2.dp)) {
+            Box(
+                modifier = Modifier
+                    .height(64.dp)
+                    .weight(1f)
+                    .align(Alignment.Bottom)
+                    .background(Color.DarkGray, shape = RoundedCornerShape(10.dp))
+            ) {
+                var lastFocusState by remember { mutableStateOf(false) }
+                BasicTextField(
+                    value = textFieldValue,
+                    onValueChange = { onTextChanged(it) },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 32.dp)
+                        .align(Alignment.CenterStart)
+                        .onFocusChanged { state ->
+                            if (lastFocusState != state.isFocused) {
+                                onTextFieldFocused(state.isFocused)
+                            }
+                            lastFocusState = state.isFocused
+                        },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = keyboardType,
+                        imeAction = ImeAction.Send
+                    ),
+                    maxLines = 1,
+                    cursorBrush = SolidColor(LocalContentColor.current),
+                    textStyle = LocalTextStyle.current.copy(color = LocalContentColor.current)
+                )
+                if (textFieldValue.text.isEmpty() && !focusState) {
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.CenterStart)
+                            .padding(start = 32.dp),
+                        text = stringResource(id = R.string.tasktextfield_hint)
+                    )
+                }
+            }
+
+        }
     }
 }
 
